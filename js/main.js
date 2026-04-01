35fba3bddf512ff02396806f10a336b1b43b8e0b
stew-ngs16 <stuo5394@gmail.com>
2026-04-01 21:47:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,15 +6,21 @@
   let nav = document.querySelector("#navArea");
   let btn = document.querySelector(".toggle-btn");
   let mask = document.querySelector("#mask");
+  let navLinks = document.querySelectorAll("#navArea a");
 
   btn.onclick = () => {
     nav.classList.toggle("open");
   };
 
   mask.onclick = () => {
-    nav.classList.toggle("open");
+    nav.classList.remove("open");
   };
 
+  navLinks.forEach((link) => {
+    link.onclick = () => {
+      nav.classList.remove("open");
+    };
+  });
 }
 
 
